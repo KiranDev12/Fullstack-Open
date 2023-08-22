0169e36980c04fdf101649a0272b6822dc61796a
KiranDev12 <kirandev.edu@gmail.com>
2023-08-22 22:35:29 +0530

    part 1.3-1.5 solution

---
+++ part1/courseinfo/src/App.jsx
@@ -0,0 +1,69 @@
+const Header = (props) => {
+  console.log(props);
+  return (
+    <div>
+      <h1>
+        {props.course.name}
+      </h1>
+    </div>
+  )
+}
+
+const Part = (props) => {
+  return (
+    <div>
+      <p>{props.name}</p>
+      <p>{props.exercises}</p>
+    </div>
+  )
+}
+const Content = (props) => {
+  return (
+    <div>
+      <Part name={props.parts[0].name} exercises={props.parts[0].exercises} />
+      <Part name={props.parts[1].name} exercises={props.parts[1].exercises} />
+      <Part name={props.parts[2].name} exercises={props.parts[2].exercises} />
+    </div>
+  )
+}
+
+const Total = (props) => {
+  let sum = 0;
+  props.parts.forEach(element => {
+    sum+=element.exercises
+  });
+  return (
+    <div>
+      <p>Total number of exercises {sum}</p>
+    </div>
+  )
+}
+const App = () => {
+  const course = {
+    name: 'Half Stack application development',
+    parts: [
+      {
+        name: 'Fundamentals of React',
+        exercises: 10
+      },
+      {
+        name: 'Using props to pass data',
+        exercises: 7
+      },
+      {
+        name: 'State of a component',
+        exercises: 14
+      }
+    ]
+  }
+
+  return (
+    <div>
+      <Header course={course} />
+      <Content parts={course.parts} />
+      <Total parts={course.parts} />
+    </div>
+  )
+}
+
+export default App
